4bcc7bff22a7c4e5d613216bb631e4863b326c5c
Tiago <tiago.portugal@ua.pt>
2022-12-21 16:57:17 +0000

    made some changes

---
+++ Linux/hypr/lg.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+waylogout \
+        --hide-cancel \
+        --screenshots \
+        --font="Baloo 2" \
+        --effect-blur=7x5 \
+        --indicator-thickness=20 \
+        --ring-color=4c566aaa \
+        --inside-color=4c566a66 \
+        --text-color=#2e3440aa \
+        --line-color=00000000 \
+        --ring-selection-color=8fbcbbaa \
+        --inside-selection-color=8fbcbb66 \
+        --text-selection-color=eaeaeaaa \
+        --line-selection-color=00000000 \
+        --poweroff-command="poweroff" \
+        --reboot-command="reboot" \
+	--suspend-command="systemctl suspend" \
+        --logout-command="loginctl terminate-session $XDG_SESSION_ID" \
+        --selection-label
